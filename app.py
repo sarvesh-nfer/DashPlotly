168ee7a026e048ab851c308d1a5b57840376dc48
Nitesh K <nitesh.k@spectralinsights.com>
2021-04-15 20:11:20 +0530

    initial commit 2

--- app.py
+++ app.py
@@ -9,3 +9,21 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 app.config.suppress_callback_exceptions = True
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/daily':
+        return daily.app.layout
+    elif pathname == '/health':
+        return health.app.layout
+    elif pathname == '/gif':
+        return gif.app.layout
+    else:
+        return home.app.layout
