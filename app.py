e751bba9e90495082e7b327f4e43c5cdff886a00
Nitesh K <nitesh.k@spectralinsights.com>
2021-04-15 20:16:55 +0530

    initial commit 3

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import dash
 import dash_bootstrap_components as dbc
-
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output, State
+from apps import health,daily,home,gif
 # bootstrap theme
 # https://bootswatch.com/lux/
 external_stylesheets = [dbc.themes.LUX]
@@ -10,20 +13,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 app.config.suppress_callback_exceptions = True
 app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    navbar,
-    html.Div(id='page-content')
+    dcc.Location('/home', refresh=False),
+    html.Div(home.app.layout)
 ])
 
-
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/daily':
-        return daily.app.layout
-    elif pathname == '/health':
-        return health.app.layout
-    elif pathname == '/gif':
-        return gif.app.layout
-    else:
-        return home.app.layout
