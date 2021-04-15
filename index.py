e3891e4df8a8453ad274a1bc99c39f9e2b9d3bae
Nitesh K <nitesh.k@spectralinsights.com>
2021-04-15 20:24:46 +0530

    initial commit 5

--- index.py
+++ index.py
@@ -1,73 +1,16 @@
+import dash
+import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
-import dash_bootstrap_components as dbc
-
-# must add this line in order for the app to be deployed successfully on Heroku
-from app import server
-from app import app
-# import all pages in the app
 from apps import health,daily,home,gif
+# bootstrap theme
+# https://bootswatch.com/lux/
+external_stylesheets = [dbc.themes.LUX]
 
-# building the navigation bar
-# https://github.com/facultyai/dash-bootstrap-components/blob/master/examples/advanced-component-usage/Navbars.py
-dropdown = dbc.DropdownMenu(
-    children=[
-        dbc.DropdownMenuItem("Home", href="/home"),
-        dbc.DropdownMenuItem("daily", href="/daily"),
-        dbc.DropdownMenuItem("health", href="/health"),
-        dbc.DropdownMenuItem("Acquisition", href="/gif"),
-    ],
-    nav = True,
-    in_navbar = True,
-    label = "Explore",
-)
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-navbar = dbc.Navbar(
-    dbc.Container(
-        [
-            html.A(
-                # Use row and col to control vertical alignment of logo / brand
-                dbc.Row(
-                    [
-                        dbc.Col(html.Img(src="/assets/spec3.png", height="30px")),
-                        dbc.Col(dbc.NavbarBrand("Report DASH", className="ml-2")),
-                    ],
-                    align="center",
-                    no_gutters=True,
-                ),
-                href="/home",
-            ),
-            dbc.NavbarToggler(id="navbar-toggler2"),
-            dbc.Collapse(
-                dbc.Nav(
-                    # right align dropdown menu with ml-auto className
-                    [dropdown], className="ml-auto", navbar=True
-                ),
-                id="navbar-collapse2",
-                navbar=True,
-            ),
-        ]
-    ),
-    color="dark",
-    dark=True,
-    className="mb-4",
-)
-
-def toggle_navbar_collapse(n, is_open):
-    if n:
-        return not is_open
-    return is_open
-
-for i in [2]:
-    app.callback(
-        Output(f"navbar-collapse{i}", "is_open"),
-        [Input(f"navbar-toggler{i}", "n_clicks")],
-        [State(f"navbar-collapse{i}", "is_open")],
-    )(toggle_navbar_collapse)
-
-# embedding the navigation bar
+server = app.server
+app.config.suppress_callback_exceptions = True
 
 
-if __name__ == '__main__':
-    app.run_server(port=8080,debug=True)
